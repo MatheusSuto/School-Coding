421a5fe7f304fb3e7756eb701549cd6fdc28bb85
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-03-06 10:22:59 -0300

    Create O número 1 é x vezes maior que o numero 2.py

---
+++ O número 1 é x vezes maior que o numero 2.py
@@ -0,0 +1,21 @@
+def Quantas_vezes_maior ():
+    n1 = int(input("Número 1: "))
+    n2 = int(input("Número 2: "))
+    n3 = int(input("Número 3: "))
+
+    if n1>n2 and n1>n3:
+        value1 = n1/n2
+        value2 = n1/n3
+        print(f'O número 1 é {value1} vezes maior que o número 2, e também {value2} vezes maior que o número 3.')
+
+    elif n2>n1 and n2>n3:
+        value1 = n2/n1
+        value2 = n2/n3
+        print(f'O número 2 é {value1} vezes maior que o número 1, e também {value2} vezes maior que o número 3.')
+
+    else:
+        value1 = n3/n1
+        value2 = n3/n2
+        print(f'O número 3 é {value1} vezes maior que o número 1, e também {value2} vezes maior que o número 3')
+
+Quantas_vezes_maior()
