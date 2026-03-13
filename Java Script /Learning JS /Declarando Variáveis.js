c29958a726012ebd02da296414da051170c10ee9
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-03-13 11:54:01 -0300

    Create Declarando Variáveis.js

---
+++ Java Script /Learning JS /Declarando Variáveis.js
@@ -0,0 +1,15 @@
+var a = 5;
+var b = 8;
+var a = 14;
+console.log(a+b);
+var c = 18;
+var d = 2**a - 2**10;
+console.log(d);
+const soma = c+d;
+const valor_1 = a;
+console.log(valor_1);
+if (valor_1 >= d){
+    console.log("Está correto");
+} else{
+    console.log("Não está correto");
+};
