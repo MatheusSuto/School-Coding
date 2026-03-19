20d80a9f896f808584ce60907418ad464324ef3a
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-03-19 08:38:01 -0300

    Add comparison script for two numbers

---
+++ Java Script /Learning JS /Maior - Menor - Igual.js
@@ -0,0 +1,9 @@
+let num_1 = parseInt(prompt("Digite o 1° número: "))
+let num_2 = parseInt(prompt("Digite o 2° número: "))
+if (num_1 > num_2){
+    console.log("O 1° número é maior que o 2°")
+} else if (num_2 > num_1) {
+    console.log("O 2° número é maior que o 1°")
+} else {
+    console.log("Os números são iguais")
+}
