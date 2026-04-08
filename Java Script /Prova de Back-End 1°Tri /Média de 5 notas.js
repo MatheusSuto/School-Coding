93a1ea00687fd7f750e7c74ecf68243805d3dbd4
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-04-08 12:12:02 -0300

    Create Média de 5 notas.js

---
+++ Java Script /Prova de Back-End 1°Tri /Média de 5 notas.js
@@ -0,0 +1,10 @@
+let n1 = parseFloat(prompt("Entre com a 1° nota"));
+let n2 = parseFloat(prompt("Entre com a 2° nota"));
+let n3 = parseFloat(prompt("Entre com a 3° nota"));
+let n4 = parseFloat(prompt("Entre com a 4° nota"));
+let n5 = parseFloat(prompt("Entre com a 5° nota"));
+
+let soma = n1 + n2 + n3 + n4 + n5
+let media = soma/5
+
+console.log("A média das notas é", media)
