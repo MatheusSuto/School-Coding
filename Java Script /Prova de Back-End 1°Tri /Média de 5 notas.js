00bc42bca4565efaeb967bd5fd9915300c8d7545
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-04-08 12:13:03 -0300

    Fix syntax errors in grade calculation

--- Java Script /Prova de Back-End 1°Tri /Média de 5 notas.js
+++ Java Script /Prova de Back-End 1°Tri /Média de 5 notas.js
@@ -4,7 +4,7 @@ let n3 = parseFloat(prompt("Entre com a 3° nota"));
 let n4 = parseFloat(prompt("Entre com a 4° nota"));
 let n5 = parseFloat(prompt("Entre com a 5° nota"));
 
-let soma = n1 + n2 + n3 + n4 + n5
-let media = soma/5
+let soma = n1 + n2 + n3 + n4 + n5;
+let media = soma/5;
 
 console.log("A média das notas é", media)
