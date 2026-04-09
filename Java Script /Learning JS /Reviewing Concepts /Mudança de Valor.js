3adff29aa3ece7698e21236b455ddc6260d19cc4
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-04-09 08:04:19 -0300

    Create Mudança de Valor.js

---
+++ Java Script /Learning JS /Reviewing Concepts /Mudança de Valor.js
@@ -0,0 +1,6 @@
+// 1. Faça um programa que exiba a seguinte mensagem: "Olá, como você está Carol?"
+
+var texto = "Olá, como você está Carol?";
+console.log(texto);
+var texto = "Eu estou lendo o livro Os Sertões de Euclides da Cunha.";
+console.log(texto)
