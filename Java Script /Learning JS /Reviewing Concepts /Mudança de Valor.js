a9c95e4d530ad97509759da4e754d5b53605678e
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-04-09 08:15:52 -0300

    Fix variable declaration and add usage examples
    Correct variable declaration and usage examples.

--- Java Script /Learning JS /Reviewing Concepts /Mudança de Valor.js
+++ Java Script /Learning JS /Reviewing Concepts /Mudança de Valor.js
@@ -2,5 +2,13 @@
 
 var texto = "Olá, como você está Carol?";
 console.log(texto);
+
 var texto = "Eu estou lendo o livro Os Sertões de Euclides da Cunha.";
+console.log(texto)
+
+// let texto = "Estudo na escola Dr. Xavier, no Rebouças";
+// console.log(texto) // Dá erro. Não se pode declarar uma variável com 2 tipos de declaração diferente
+
+//O certo seria:
+texto = "Estudo na escola Dr. Xavier, no Rebouças"
 console.log(texto)
