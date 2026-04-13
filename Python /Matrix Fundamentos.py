5eba0d52a1639a96f9036413c8a6cdc24f662cd6
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-04-13 08:49:24 -0300

    Implement matrix generation and number inversion
    Added a matrix generation and number inversion example.

---
+++ Python /Matrix Fundamentos.py
@@ -0,0 +1,28 @@
+matrix = []
+for i in range (6):
+    for j in range (6):
+        print(i*j+1, end = " ")
+    print()
+print()
+
+'''
+1 1 1 1 1 1 
+1 2 3 4 5 6
+1 3 5 7 9 11
+1 4 7 10 13 16
+1 5 9 13 17 21
+1 6 11 16 21 26
+'''
+
+
+
+numeros = [9, 7, 6, 23, 12, 7]
+numInvertidos = numeros[: :-1]
+print(numeros) #[9, 7, 6, 23, 12, 7]
+print(numInvertidos) #[7, 12, 23, 6, 7, 9]
+somaNumeros = 0
+for ind in range (): #figure out what the fuck comes here ye
+    print(numeros[ind], end = " ")
+    somaNumeros += numeros[ind]
+print()
+print(somaNumeros)
