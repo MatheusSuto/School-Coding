1c764f32ba6b1194e999c77531c38a02e35fc2f4
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-05-29 09:17:47 -0300

    Create Lista de presentes.py

---
+++ Python /Lista de presentes.py
@@ -0,0 +1,11 @@
+"""
+Seja uma lsita de presentes: bola, lápis, caderno, skate e patins. Você deve fazer um programa em python para que aleatoriamente para que o presente escolhido seja um deles e somente um.
+"""
+
+import random as rd
+
+listaPresentes = ["bola", "lápis", "caderno", "skate", "patins"]
+
+escolha = rd.randrange(1, 6)
+
+print(f"O presente escolhido foi {listaPresentes[escolha]}")
