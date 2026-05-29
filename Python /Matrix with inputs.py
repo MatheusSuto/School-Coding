e481838cdfa3f076ff5cb55ff6eede9fcd9591e8
MatheusSuto <matheus.suto@escola.pr.gov.br>
2026-05-29 10:47:17 -0300

    Create Matrix with inputs.py

---
+++ Python /Matrix with inputs.py
@@ -0,0 +1,37 @@
+"""
+Construa uma matriz com a seguinte formação:
+
+|5            |
+|6 12         |
+|7 14 21      |
+|8 16 24 32   |
+|9 18 27 36 45|
+"""
+
+
+def matriz(size):
+    for i in range(size + 1):
+        for j in range(size + 1):
+
+            if j > i:
+                continue
+            elif j == j:
+                print((j + 1) * (i + 5), end=" ")
+                """
+                elif j == 0:
+                    print(i + 5, end=" ")
+                elif j == 1:
+                    print(2 * (i + 5), end=" ")
+                elif j == 2:
+                    print(3 * (i + 5), end=" ")
+                elif j == 3:
+                    print(4 * (i + 5), end=" ")
+                elif j == 4:
+                    print(5 * (i + 5), end=" ")
+                """
+        print()
+
+
+tamanho = input("Digite o tamanho da matriz: ")
+tamanho = int(tamanho)
+print(matriz(tamanho))
